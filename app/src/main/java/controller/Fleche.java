4c5bb3cc4175d3010be0e276dd78e0435dd08e43
Youkuulele <ramadi.younes@gmail.com>
2021-06-19 17:08:15 +0200

    zebi le commit

--- app/src/main/java/controller/Fleche.java
+++ app/src/main/java/controller/Fleche.java
@@ -1,23 +1,16 @@
 package controller;
 
-import com.example.apadnom.R;
-
-public class Fleche extends Pion {
-    public Fleche(byte pCouleur, byte direction) {
+public class Fleche extends Pion{
+    public Fleche(byte pCouleur, byte direction){
         super(pCouleur, direction);
-        if (color == 0)
-            super.setImg(R.drawable.blue_arrow);
-        else
-            super.setImg(R.drawable.red_arrow);
     }
 
-    public Fleche() {
+    public Fleche(){
         super();
     }
 
-
-    public String toString() {
-        if (this.color == 0) {
+    public String toString(){
+        if(this.color == 0){
             return "Fb";
         }
         return "Fn";
