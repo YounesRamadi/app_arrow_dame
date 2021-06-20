ed2c4b5f45ed72bfe75a00d8025f89dc1069d692
Gabriel <Gabriel.pithon@outlook.fr>
2021-06-20 20:37:47 +0200

    debut pour alpha beta

--- src/IA/Ia.java
+++ src/IA/Ia.java
@@ -51,19 +51,19 @@ public class Ia {
         return score;
     }
 
-    public int max(Jeu j, int ligne, int colonne, int lim){
+    public int[] max(Jeu j, int ligne, int colonne, int lim, int alpha, int beta){
         j.move(ligne, colonne);
-        int max=0;
-        int inter=0;
+        int[] max={0,alpha,beta};
+        int[] inter={0,alpha,beta}; //600 --> alpha | -600 --> beta
         int obl=0;
         int[][] poss;
         if(j.get_hasJumped()==1){
             poss=j.get_possibilities(j.get_plateau()[ligne][colonne], ligne, colonne);
             if(poss!=null){
                 for(int i=0; i<poss.length; i++){
-                    inter=max(j,poss[i][0],poss[i][1],lim);
-                    if(max<inter){
-                        max=inter;
+                    inter=max(j,poss[i][0],poss[i][1],lim, inter[1], inter[2]);
+                    if(max[0]<inter[0]){
+                        max[0]=inter[0];
                     }
                 }
             }
@@ -74,9 +74,9 @@ public class Ia {
                     if(j.get_plateau()[lig][col].get_color()==j.get_plateau()[ligne][colonne].get_color() && j.get_plateau()[lig][col] instanceof Etoile){
                         poss=j.get_possibilities(j.get_plateau()[lig][col], lig, col);
                         for(int i=0; i<poss.length; i++){
-                            inter=max(j,poss[i][0],poss[i][1],lim);
-                            if(max<inter)
-                                max=inter;
+                            inter=max(j,poss[i][0],poss[i][1],lim, inter[1], inter[2]);
+                            if(max[0]<inter[0])
+                                max[0]=inter[0];
                         }
                     }
                 }
@@ -88,16 +88,17 @@ public class Ia {
                     if(j.get_plateau()[lig][col].get_color()==j.get_plateau()[ligne][colonne].get_color() && j.get_plateau()[lig][col] instanceof Etoile){
                         poss=j.get_possibilities(j.get_plateau()[lig][col], lig, col);
                         for(int i=0; i<poss.length; i++){
-                            inter=min(j,poss[i][0],poss[i][1],lim);
-                            if(max<inter)
-                                max=inter;
+                            inter=min(j,poss[i][0],poss[i][1],lim, inter[1], inter[2]);
+                            if(max[0]<inter[0])
+                                max[0]=inter[0];
                         }
                     }
                 }
             }
         }
         if(lim==0 || j.get_nb_et_blanc()==0 || j.get_nb_et_noir()==0){
-            return valuation(j, (byte)((j.get_plateau()[ligne][colonne].get_color()+1)%2));
+            max[0]=valuation(j, (byte)((j.get_plateau()[ligne][colonne].get_color()+1)%2));
+            return max;
         }
         if(j.get_jump()==0){
             j.set_hasJumped((byte) 0);
@@ -109,25 +110,25 @@ public class Ia {
                             for(int n=0; n<j.get_possible_jump().length; n++){
                                 if ((j.get_plateau()[lig + (j.get_possible_jump()[n][0] - lig)/2][col+(j.get_possible_jump()[n][1] - col)/2].get_color()) != j.get_plateau()[lig][col].get_color()){
                                     if(obl==0)
-                                        max=0;
-                                    inter=max(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim);
-                                    if(max<inter)
-                                        max=inter;
+                                        max[0]=0;
+                                    inter=max(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim, inter[1], inter[2]);
+                                    if(max[0]<inter[0])
+                                        max[0]=inter[0];
                                     obl=1;
                                 }
                                 else if(obl==0){
-                                    inter=max(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim);
-                                    if(max<inter)
-                                        max=inter;
+                                    inter=max(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim, inter[1], inter[2]);
+                                    if(max[0]<inter[0])
+                                        max[0]=inter[0];
                                 }
                             }
                         }
                     }
                     if(j.get_possible_move()!=null && obl==0){
                         for(int n=0; n<j.get_possible_move().length; n++){
-                            inter=min(j,j.get_possible_move()[n][0],j.get_possible_move()[n][0],lim-1);
-                            if(max<inter)
-                                max=inter;
+                            inter=min(j,j.get_possible_move()[n][0],j.get_possible_move()[n][0],lim-1, inter[1], inter[2]);
+                            if(max[0]<inter[0])
+                                max[0]=inter[0];
                         }
                     }
                     
@@ -137,19 +138,19 @@ public class Ia {
         return max;
     }
 
-    public int min(Jeu j, int ligne, int colonne, int lim) {
+    public int[] min(Jeu j, int ligne, int colonne, int lim, int alpha, int beta) {
         j.move(ligne, colonne);
-        int min=0;
-        int inter=0;
+        int[] min={0,alpha,beta};
+        int[] inter={0,alpha,beta};
         int obl=0;
         int[][] poss;
         if(j.get_hasJumped()==1){
             poss=j.get_possibilities(j.get_plateau()[ligne][colonne], ligne, colonne);
             if(poss!=null){
                 for(int i=0; i<poss.length; i++){
-                    inter=min(j,poss[i][0],poss[i][1],lim);
-                    if(min>inter){
-                        min=inter;
+                    inter=min(j,poss[i][0],poss[i][1],lim, inter[1], inter[2]);
+                    if(min[0]>inter[0]){
+                        min[0]=inter[0];
                     }
                 }
             }
@@ -160,9 +161,9 @@ public class Ia {
                     if(j.get_plateau()[lig][col].get_color()==j.get_plateau()[ligne][colonne].get_color() && j.get_plateau()[lig][col] instanceof Etoile){
                         poss=j.get_possibilities(j.get_plateau()[lig][col], lig, col);
                         for(int i=0; i<poss.length; i++){
-                            inter=min(j,poss[i][0],poss[i][1],lim);
-                            if(min>inter)
-                                min=inter;
+                            inter=min(j,poss[i][0],poss[i][1],lim, inter[1], inter[2]);
+                            if(min[0]>inter[0])
+                                min[0]=inter[0];
                         }
                     }
                 }
@@ -174,16 +175,17 @@ public class Ia {
                     if(j.get_plateau()[lig][col].get_color()==j.get_plateau()[ligne][colonne].get_color() && j.get_plateau()[lig][col] instanceof Etoile){
                         poss=j.get_possibilities(j.get_plateau()[lig][col], lig, col);
                         for(int i=0; i<poss.length; i++){
-                            inter=max(j,poss[i][0],poss[i][1],lim);
-                            if(min>inter)
-                                min=inter;
+                            inter=max(j,poss[i][0],poss[i][1],lim, inter[1], inter[2]);
+                            if(min[0]>inter[0])
+                                min[0]=inter[0];
                         }
                     }
                 }
             }
         }
         if(lim==0 || j.get_nb_et_blanc()==0 || j.get_nb_et_noir()==0){
-            return valuation(j, j.get_plateau()[ligne][colonne].get_color());
+            min[0]=valuation(j, j.get_plateau()[ligne][colonne].get_color());
+            return min;
         }
         if(j.get_jump()==0){
             j.set_hasJumped((byte) 0);
@@ -195,25 +197,25 @@ public class Ia {
                             for(int n=0; n<j.get_possible_jump().length; n++){
                                 if ((j.get_plateau()[lig + (j.get_possible_jump()[n][0] - lig)/2][col+(j.get_possible_jump()[n][1] - col)/2].get_color()) != j.get_plateau()[lig][col].get_color()){
                                     if(obl==0)
-                                        min=0;
-                                    inter=min(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim);
-                                    if(min>inter)
-                                        min=inter;
+                                        min[0]=0;
+                                    inter=min(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim, inter[1], inter[2]);
+                                    if(min[0]>inter[0])
+                                        min[0]=inter[0];
                                     obl=1;
                                 }
                                 else if(obl==0){
-                                    inter=min(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim);
-                                    if(min>inter)
-                                        min=inter;
+                                    inter=min(j,j.get_possible_jump()[n][0],j.get_possible_jump()[n][1],lim, inter[1], inter[2]);
+                                    if(min[0]>inter[0])
+                                        min[0]=inter[0];
                                 }
                             }
                         }
                     }
                     if(j.get_possible_move()!=null && obl==0){
                         for(int n=0; n<j.get_possible_move().length; n++){
-                            inter=max(j,j.get_possible_move()[n][0],j.get_possible_move()[n][0],lim-1);
-                            if(min>inter)
-                                min=inter;
+                            inter=max(j,j.get_possible_move()[n][0],j.get_possible_move()[n][0],lim-1, inter[1], inter[2]);
+                            if(min[0]>inter[0])
+                                min[0]=inter[0];
                         }
                     }
                     
@@ -226,16 +228,19 @@ public class Ia {
     public int[] minMax(byte couleur, Jeu j, int lim) {
         Pion[][] plateau = j.get_plateau();
         int[] pos= {-1,-1};
+        int alpha=600;
+        int beta=-600;
         int max=0;
-        int inter=0;
+        int[] inter= {0,alpha,beta};
+        inter[0]=inter[1]+inter[2];                     //  !!!!!!!!!!!!!!!!!! ne sert à rien juste pour les warnings
         for (int ligne = 0; ligne < 7; ligne++) {
             for (int col = 0; col < 9; col++) {
                 if (plateau[ligne][col].get_color() == couleur) {
                     int[][] poss=j.get_possibilities(plateau[ligne][col],ligne,col);
                     for(int s=0; s<poss.length; s++){
-                        inter=min(j,poss[s][0],poss[s][1],lim);
-                        if(max<inter){
-                            max=inter;
+                        inter=min(j,poss[s][0],poss[s][1],lim,alpha, beta);
+                        if(max<inter[0]){
+                            max=inter[0];
                             pos=poss[s];
                         }
                     }
