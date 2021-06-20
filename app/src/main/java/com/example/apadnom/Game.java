206083690a32688f52912fad0ee7318781eb4b2c
Youkuulele <ramadi.younes@gmail.com>
2021-06-20 18:53:58 +0200

    fix déplacements et affichage

--- app/src/main/java/com/example/apadnom/Game.java
+++ app/src/main/java/com/example/apadnom/Game.java
@@ -7,11 +7,11 @@ public class Game {
     private int turn;
     private int jump;
     private int[][] possible_jump;
-    private  int[][] possible_move;
+    private int[][] possible_move;
     private byte has_jumped;
     private int[] selected = new int[2];
 
-    public void Game(){
+    public void Game() {
         turn = 0;
         jump = 0;
         has_jumped = (byte) 0;
