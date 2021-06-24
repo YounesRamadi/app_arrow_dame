d298852f1a50f24cad25b484d58a7b74225a3f3d
YouKuulele <ramadi.younes@gmail.com>
2021-06-25 01:01:22 +0200

    adding border

--- app/src/main/java/controller/History.java
+++ app/src/main/java/controller/History.java
@@ -1,5 +1,12 @@
 package controller;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class History {
+    private File f = new File("");
+    File file = new File("test");
+    Path path = Paths.get(file.getAbsolutePath());
 
 }
